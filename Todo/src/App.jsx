08e330c62d741e7374aaec238575e8b832b40f70
Vinicius Nunes <v.nunhez27@gmail.com>
2023-04-17 15:52:50 -0300

    Feature: empty task field

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -3,12 +3,15 @@ import { Header } from "./components/Header";
 import styles from "./App.module.css";
 
 import Plus from "./assets/Plus.svg";
+import IconEmpty from './assets/Iconempty.svg'
 
 import "./global.css";
 
 function App() {
   return (
     <>
+  
+  <div className={styles.container}>
       <Header />
 
       <form className={styles.taskbar}>
@@ -17,6 +20,21 @@ function App() {
           Criar <img src={Plus} alt="Icone de +" />
         </button>
       </form>
+
+     <div className={styles.infoTask}>
+      <p className={styles.infoCreate}>Tarefas criadas  <span>0</span></p>
+      <p className={styles.infoDone}>Concluídas  <span>0</span> </p> 
+    </div> 
+  
+
+ <div className={styles.cardInfo}>
+     <div className={styles.infoEmpty}>
+      <img src={IconEmpty} alt="Icon Lista Vazia" />
+        <p>Você ainda não tem tarefas cadastradas</p>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+      </div>
+     </div>
+   </div>
     </>
   );
 }
