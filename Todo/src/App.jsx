405ba1601ad49315cc8db0478588470a09bc40a2
Vinicius Nunes <v.nunhez27@gmail.com>
2023-04-25 14:45:49 -0300

    Feat:  ADD NEW TASK

--- Todo/src/App.jsx
+++ Todo/src/App.jsx
@@ -1,108 +1,90 @@
-import { v4 as uuidv4 } from 'uuid';
-
+import "./global.css";
+import IconEmpty from './assets/Iconempty.svg'
+import Plus from "./assets/Plus.svg";
+import styles from "./App.module.css";
 import { Header } from "./components/Header";
 import { Task } from "./components/Task";
 import { useState } from 'react';
-
-import styles from "./App.module.css";
-
-import Plus from "./assets/Plus.svg";
-import IconEmpty from './assets/Iconempty.svg'
-
-import "./global.css";
-
+import { v4 as uuidv4 } from 'uuid';
 
 export function App() {
+  const [newTask, setNewTask] = useState('');
+  const [tasks, setTasks] = useState([
+    {
+      id: uuidv4(),
+      title: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isComplete: false
+    },
+    {
+      id: uuidv4(),
+      title: 'Estudar Javascript',
+      isComplete: true
+    },
+    {
+      id: uuidv4(),
+      title: 'Estudar React',
+      isComplete: false
+    }
+  ]);
 
- const [newtask, setNewTask] = useState ([
- 
-  {
-    id:uuidv4 (),
-    title:'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
-    isComplete:false
-   },
-  {
-   id:uuidv4(),
-   title:'Estudar Javascript',
-   isComplete:true
- },
- {
-   id:uuidv4(),
-   title:'Estudar React',
-   isComplete:false
-  }
+  function handleInputChange(e) {
+    setNewTask(e.target.value);
+  };
 
-
-]) 
-
-const [newTextTask, setNewTextTask] = useState('')
-
-
-function handleCreateNewTask() {
-     event.preventDefault()
-
-     const newTaskText = event.target.text.value
-
-     setNewTask([...newtask, newTextTask])
-
-     setNewTextTask('')
-  } 
-
- function handleNewTextChange () {
-  setNewTextTask(event.target.value)
- } 
-
-  
+  function handleCreateNewTask(e) {
+    e.preventDefault();
+    setTasks([
+      ...tasks,
+      {
+        id: uuidv4(),
+        title: newTask,
+        isComplete: false
+      }
+    ]);
+    setNewTask('');
+  };
   return (
-    <>
-  
-  <div className={styles.container}>
+    <div className={styles.container}>
       <Header />
 
-      <form  onSubmit={handleCreateNewTask} className={styles.taskbar}>
-       
-       <textarea 
-        name='text'
-        placeholder="Adicione uma nova tarefa" 
-        onChange={handleNewTextChange}
-        value={newTextTask}
+      <form onSubmit={handleCreateNewTask} className={styles.taskbar}>
+        <textarea
+          name='text'
+          placeholder="Adicione uma nova tarefa"
+          onChange={handleInputChange}
+          value={newTask}
         />
-       
         <button>
           Criar <img src={Plus} alt="Icone de +" />
         </button>
       </form>
 
-     <div className={styles.infoTask}>
-      <p className={styles.infoCreate}>Tarefas criadas  <span>0</span></p>
-      <p className={styles.infoDone}>Concluídas  <span>0</span> </p> 
-    </div> 
-  
-
- <div className={styles.cardInfo}>
-     <div className={styles.infoEmpty}>
-      <img src={IconEmpty} alt="Icon Lista Vazia" />
-        <p>Você ainda não tem tarefas cadastradas</p>
-        <p>Crie tarefas e organize seus itens a fazer</p>
+      <div className={styles.infoTask}>
+        <p className={styles.infoCreate}>
+          Tarefas criadas <span>0</span>
+        </p>
+        <p className={styles.infoDone}>
+          Concluídas <span>0</span>
+        </p>
       </div>
-     </div>
-   </div>
 
-  {newtask.map( task => {
-    return (
-      <Task 
-      key={task.id}
-      title={task.title}
-      isChecked={task.isComplete}
+      <div className={styles.cardInfo}>
+        <div className={styles.infoEmpty}>
+          <img src={IconEmpty} alt="Icon Lista Vazia" />
+          <p>Você ainda não tem tarefas cadastradas</p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </div>
 
-      />
-    )
-  })} 
-
-    </>
-
-
+      {tasks.map(task => (
+        <Task
+          key={task.id}
+          title={task.title}
+          isChecked={task.isComplete}
+        />
+      ))}
+    </div>
   );
-}
+};
 
 export default App;
